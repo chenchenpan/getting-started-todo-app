e766a4883c4f36c2e575d5f4ebb8eaaf5c38216a
chenchenpan <pcc.pku@gmail.com>
2024-09-22 15:46:28 -0700

    Add some changes for greetings, background color.

--- backend/src/routes/getGreeting.js
+++ backend/src/routes/getGreeting.js
@@ -1,7 +1,14 @@
-const GREETING = 'Hello world!';
+// const GREETING = 'Hello world!';
+const GREETINGS = [
+    "Whalecome!",
+    "All hands on deck!",
+    "Charting the course ahead!",
+    "世界你好!",
+];
 
 module.exports = async (req, res) => {
     res.send({
-        greeting: GREETING,
+        // greeting: GREETING,
+        greeting: GREETINGS[Math.floor(Math.random() * GREETINGS.length)],
     });
 };
